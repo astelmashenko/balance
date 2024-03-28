8da237826b754bfbee86296342f95785a73fcc57
astelmashenko <andrey.stelmashenko@gmail.com>
2024-03-28 18:01:35 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -18,20 +18,17 @@ use stm32f1xx_hal::{
     prelude::*,
     timer::{CounterMs, Event},
 };
+use shared_bus_rtic::SharedBus;
 
 type LedPin = gpio::PC13<Output<PushPull>>;
 type BlockingI2cPB89 = i2c::BlockingI2c<pac::I2C1, (Pin<'B', 8, Alternate<OpenDrain>>, Pin<'B', 9, Alternate<OpenDrain>>)>;
-// type I2cDisplay = Ssd1306<I2CInterface<Ssd1306<I2CInterface<shared_bus::I2cProxy<'static, shared_bus::NullMutex< BlockingI2cPB89 >>>, DisplaySize128x64, TerminalMode>>, DisplaySize128x64, TerminalMode>;
-type I2cDisplay = Ssd1306<I2CInterface<shared_bus_rtic::SharedBus<BlockingI2cPB89>>, DisplaySize128x64, TerminalMode>;
-// type I2cDisplay = Ssd1306<I2CInterface<BlockingI2cPB89>, DisplaySize128x64, TerminalMode>;
-type I2cMpu6050 = Mpu6050<shared_bus_rtic::SharedBus<BlockingI2cPB89>>;
+type I2cDisplay = Ssd1306<I2CInterface<SharedBus<BlockingI2cPB89>>, DisplaySize128x64, TerminalMode>;
+type I2cMpu6050 = Mpu6050<SharedBus<BlockingI2cPB89>>;
 
 // Create a Global Variable for the Timer Peripheral that I'm going to pass around.
 static G_TIM: Mutex<RefCell<Option<CounterMs<TIM2>>>> = Mutex::new(RefCell::new(None));
 // Create a Global Variable for the LED GPIO Peripheral that I'm going to pass around.
 static G_LED: Mutex<RefCell<Option<LedPin>>> = Mutex::new(RefCell::new(None));
-
-// static G_I2C2: Mutex<RefCell<Option<BlockingI2cPB89>>> = Mutex::new(RefCell::new(None));
 
 static G_DISP: Mutex<RefCell<Option<I2cDisplay>>> = Mutex::new(RefCell::new(None));
 static G_MPU: Mutex<RefCell<Option<I2cMpu6050>>> = Mutex::new(RefCell::new(None));
@@ -43,19 +40,17 @@ fn main() -> ! {
     // Get access to the device specific peripherals from the peripheral access crate
     let dp = pac::Peripherals::take().unwrap();
 
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
+    // Take ownership over the raw flash and rcc devices and convert them into the corresponding HAL structs
     let mut flash = dp.FLASH.constrain();
     let rcc = dp.RCC.constrain();
 
-    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-    // `clocks`
+    // Freeze the configuration of all the clocks in the system and store the frozen frequencies
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
     // Configure the syst timer to trigger an update every second
     // let mut sys_timer = Timer::syst(cp.SYST, &clocks).counter_hz();
     let mut timer = dp.TIM2.counter_ms(&clocks);
-    timer.start(2000.millis()).unwrap();
+    timer.start(500.millis()).unwrap();
 
     // Set up to generate interrupt when timer expires
     timer.listen(Event::Update);
@@ -92,9 +87,6 @@ fn main() -> ! {
         1000,
     );
 
-    // let shared_i2c = RefCell::new(i2c_2);
-    // let i2c_device = hbus::i2c::RefCellDevice::new(&shared_i2c);
-    // let i2c_bus = shared_bus::BusManagerSimple::new(i2c_2);
     let i2c_sbus = shared_bus_rtic::new!(i2c_2, BlockingI2cPB89);
     let interface = I2CDisplayInterface::new(i2c_sbus.acquire());
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0).into_terminal_mode();
