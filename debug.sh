0b23abfbc2b74ce81d5f9a20820641d5ddcfa27b
Andrii Stelmashenko <andrey.stelmashenko@gmail.com>
2026-01-26 23:44:28 +0200

    added debug command for macos

--- debug.sh
+++ debug.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-gdb-multiarch -q -ex "target remote :3333" target/thumbv7m-none-eabi/debug/balance
+# gdb-multiarch -q -ex "target remote :3333" target/thumbv7m-none-eabi/debug/balance
+#
+
+arm-none-eabi-gdb -q -ex "target remote :3333" target/thumbv7m-none-eabi/debug/balance
