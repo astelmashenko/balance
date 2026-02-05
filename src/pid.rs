7a851191a2b606dbb6d28d3f941dd64657fd7e1a
Andrii Stelmashenko <andrey.stelmashenko@gmail.com>
2026-02-05 13:01:56 +0200

    tuning

--- src/pid.rs
+++ src/pid.rs
@@ -65,9 +65,9 @@ impl BalancePD {
     /// Create with original default gains
     pub fn new(center_gravity: f32) -> Self {
         Self {
-            kp: 700.0,
-            ki: 0.1, // 0.0
-            kd: 1.5, // 4.0
+            kp: 500.0,
+            ki: 1.5, // 0.0
+            kd: 1.8, // 4.0
             center_gravity,
             integral: 0.0,
             integral_limit: 30000.0,
@@ -110,8 +110,8 @@ pub struct VelocityPI {
 impl VelocityPI {
     pub fn new() -> Self {
         Self {
-            kp: 17.0, //-600
-            ki: 0.1,  // -0.5
+            kp: 8.0,   //-600
+            ki: 0.005, // -0.5
             filtered_encoder: 0.0,
             integral: 0.0,
             integral_limit: 10000.0,
